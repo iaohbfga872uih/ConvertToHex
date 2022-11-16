79b9b04522b1aa435ba8ad77bb99228aaa6f148d
Administrator <Administrator@MININT-9RTKJTK>
2022-11-16 12:47:12 -0700

    Done Commit

--- Program.cs
+++ Program.cs
@@ -1,1 +1,35 @@
 using System;
+using System.ComponentModel;
+using System.Text;
+
+namespace Monkey
+{
+    class MonkeyProgram {
+
+        static void Main(string[] args)
+        {
+
+            string test = "Big Boi";
+            Console.WriteLine(ConvertToHex(test));
+
+        }
+
+        static string ConvertToHex(string ToConvert)
+        {   
+
+            
+            byte[] ConvertedBytes = Encoding.UTF8.GetBytes(ToConvert);
+
+
+            string Converted = BitConverter.ToString(ConvertedBytes);
+            Converted = Converted.Replace("-", " ");
+            
+
+            return Converted;
+
+        }
+
+    
+    }
+
+}
